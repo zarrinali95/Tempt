b0f9254a9b7d0d2c56a973bc7ade1be8f9bc9c76
Demo User <demouser@Jonny-Wangs-Laptop.local>
2018-07-24 11:36:38 -0400

    timer works in popup html

---
+++ timer.js
@@ -0,0 +1,32 @@
+var timeoutHandle;
+function countdown() {
+    var seconds = 60;
+    var mins = document.getElementById('min');
+    var num_min = Number(mins.value);
+    function tick() {
+        var counter = document.getElementById("timer");
+        var current_minutes = num_min-1
+        seconds--;
+
+        counter.innerHTML =
+        current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+
+        if( seconds > 0 ) {
+            timeoutHandle=setTimeout(tick, 1000);
+        } else {
+
+            if(mins > 1){
+
+         setTimeout(function () { countdown(mins - 1); }, 1000);
+
+            }
+        }
+    }
+  tick();
+}
+
+// countdown();
+
+// <div>
+//	<input type='number' id ='sec' placeholder='Number of seconds'/>
+// </div>
