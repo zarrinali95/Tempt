5dd5f50742fc38071b1118c9379c10d6018655db
Demo User <demouser@Jonny-Wangs-Laptop.local>
2018-07-24 17:15:45 -0400

    pause button works!!!

--- timer.js
+++ timer.js
@@ -4,6 +4,9 @@ var njum_mins=0;
 var counter =0;
 var current_minutes =0;
 var seconds = 60;
+var pauseBool = false;
+var pressOnce=false;
+
 
 window.onload = function() {
 
@@ -11,7 +14,7 @@ window.onload = function() {
 
         document.getElementById('timerButton').addEventListener('click', tick);
         document.getElementById('resetButton').addEventListener('click', reset);
-      //  document.getElementById('pauseButton').addEventListener('click', pause);
+        document.getElementById('pauseButton').addEventListener('click', pause);
 
       });
 
@@ -28,21 +31,41 @@ function reset(){
   seconds = 60;
 }
 
-
+function pause(){
+  console.log("pause works")
+  if (pauseBool==false){
+    pauseBool=true;
+  }else{
+    pauseBool=false;
+  }
+  if (pauseBool==false){
+    tick();
+  }
+}
 function tick() {
-    console.log(seconds)
-    seconds--;
-
-    counter.innerHTML =
-    current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-
-    if( seconds > 0 ) {
-        setTimeout(tick, 1000);
-    } else {
-
-        if(mins > 1){
-     setTimeout(function () { countdown(mins - 1); }, 1000);
-
-        }
+    if (pauseBool==true){
+      return true;
     }
+      mins = document.getElementById('min');
+      num_min = Number(mins.value);
+      counter = document.getElementById("timer");
+      current_minutes = num_min-1;
+      console.log(seconds)
+
+      seconds--;
+
+
+      counter.innerHTML =
+      current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+
+        if( seconds > 0 ) {
+            setTimeout(tick, 1000);
+        } else {
+
+            if(mins > 1){
+         setTimeout(function () { countdown(mins - 1); }, 1000);
+
+            }
+
+      }
 }
