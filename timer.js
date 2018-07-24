27175743be4f9430896e5068cdea92f23d29e296
Demo User <demouser@MacBook-Pro.local>
2018-07-24 11:19:44 -0400

    changed html and css of popup

--- timer.js
+++ timer.js
@@ -1,12 +1,17 @@
-var interval = setInterval(time, 1000);
+
+function interval() {
+  setInterval(time, 60000);
+}
+//time function called every 60 seconds
 
 function time() {
+  //this is the users input in minutes returned as a value
   var distance = parseInt(document.getElementById("minutes").value);
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
-
-  document.getElementById("demo").innerHTML =  minutes + "m ";
+  var minutes =
+  document.getElementById("demo").innerHTML =  minutes + "minutes left";
+  //if countdown is over, write EXPIRED
   if (distance < 0) {
-        clearInterval(interval);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-      }
+      clearInterval(interval);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+  }
 }
