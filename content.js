69af9d41e0382e309bba9111c26372de088c71f0
Demo User <demouser@MacBook-Pro.local>
2018-07-24 10:32:03 -0400

    added a timer- does not work

--- content.js
+++
@@ -1,1 +0,0 @@
-// alert("The alert works from content.js")
--- timer.js
+++ timer.js
@@ -1,1 +1,12 @@
-print(placeholder)
+var interval = setInterval(time, 1000);
+
+function time() {
+  var distance = parseInt(document.getElementById("minutes").value);
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+
+  document.getElementById("demo").innerHTML =  minutes + "m ";
+  if (distance < 0) {
+        clearInterval(interval);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+}
